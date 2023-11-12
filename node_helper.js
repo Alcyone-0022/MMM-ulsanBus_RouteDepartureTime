962f9823724ed48a8d3fc984d4c0335d9bb25265
ExyKnox <hskhsk22000@gmail.com>
2023-11-12 14:09:21 +0900

    changed function name to proper role

--- node_helper.js
+++ node_helper.js
@@ -1,27 +1,29 @@
 var NodeHelper = require("node_helper");
-let {PythonShell} = require('python-shell')
+const fetch = require("node-fetch");
 
 module.exports = NodeHelper.create({
-  start: function () {
-
-  },
-  notificationReceived: function() {},
-  socketNotificationReceived: function(notification, payload) {
-	  var self = this;
-	  switch(notification){
-		  case "BUSSTOP_DATA_REQUEST":
-			  
-			let pyshell = new PythonShell('../MagicMirror/modules/MMM-ulsanBus/ulsan_busArrivalInfo.py');
-			// pyshell.defaultOptions = { mode: 'json' };
-			pyshell.send(payload);
-
-			pyshell.once('message', function (message) {
-			  // received a message sent from the Python script (a simple "print" statement)
-			  // Log.log(message);
-				// console.log('\"' + message + '\"');
-				self.sendSocketNotification("DATA", message);
-			});
-	  }  
-  },
-
+	start: function () {},
+	notificationReceived: function() {},
+	socketNotificationReceived: function(notification, payload) {
+		switch(notification){
+			case 'TIMETABLE_REQ':
+				this.getTimeTable(payload[0], payload[1], payload[2]);
+		}
+	},
+	async getTimeTable(key, routeNumber, dayOfWeek){
+		// https://stackoverflow.com/questions/12460378/how-to-get-json-from-url-in-javascript (14 참고)
+		var url = 'http://openapi.its.ulsan.kr/UlsanAPI/BusTimetable.xo'
+		var queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + key; /*Service Key*/
+		queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('200'); /**/
+		queryParams += '&' + encodeURIComponent('routeNo') + '=' + encodeURIComponent(routeNumber); /**/
+		queryParams += '&' + encodeURIComponent('dayOfWeek') + '=' + encodeURIComponent(dayOfWeek); /**/
+		let obj = null;
+		try {
+			obj = await (await fetch(url + queryParams)).json();
+		} catch(e) {
+			obj = {'Error': 'Error in fetching data'};
+		}
+		this.sendSocketNotification('TIMETABLE_RECV', obj);
+		return obj;
+	},
 });
