ed179ac72f27667568c4bd8e2683c3f0db308ee8
ExyKnox <hskhsk22000@gmail.com>
2023-11-13 11:49:55 +0900

    todo comment

--- MMM-ulsanBus_RouteDepartureTime.js
+++ MMM-ulsanBus_RouteDepartureTime.js
@@ -26,7 +26,8 @@ Module.register("MMM-ulsanBus_RouteDepartureTime", {
                 self.sendSocketNotification("ROUTEDEPARTURETIME_MODULE_READY", self.config.key)
                 break;
             case "CLOCK_MINUTE":
-                Log.log(payload);
+                // 가장 최근 버스 출발시간과 현재 분을 비교해서 다음 시간 업데이트 여부 판독
+                // 막차 이후일 때 - 배열 길이가 1일 때에는? 내일까지 남은 시간만큼 setTimeout 설정하고, 트리거 시 해당 노선 TIMETABLE_REQ
                 break;
         }
     },
