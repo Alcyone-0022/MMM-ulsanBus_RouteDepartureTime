709a3acf3c6d10c33933624504dd5205affb954c
ExyKnox <hskhsk22000@gmail.com>
2023-11-15 17:58:42 +0900

    removed unused lines

--- MMM-ulsanBus_RouteDepartureTime.js
+++ MMM-ulsanBus_RouteDepartureTime.js
@@ -35,16 +35,6 @@ Module.register("MMM-ulsanBus_RouteDepartureTime", {
                     self.checkRouteTime(self.currentTimeTables);
                 }, 5000);
                 break;
-            case "CLOCK_MINUTE":
-                // 가장 최근 버스 출발시간과 현재 분을 비교해서 다음 시간 업데이트 여부 판독
-                // 막차 이후일 때 - 내일까지 남은 시간만큼 setTimeout 설정하고, 트리거 시 해당 노선 TIMETABLE_REQ
-
-                // 이거 https://stackoverflow.com/questions/20502277/call-function-on-the-minute-every-minute 로 바꾸자
-                // 타이머 트리거 시점은 DOM_OBJECTS_CREATED, 호출할 함수는 nullsafe하게, DOM 직접 접근해서 수정하고
-                // 자정이 넘어갔는지 판독도 분당 호출되는 함수에서?
-
-                //근데, 사실... 이거 그렇게 time critical 하지는 않다....
-                break;
         }
     },
     getTimesFromNow: function(timetables, quantity) {
@@ -82,7 +72,7 @@ Module.register("MMM-ulsanBus_RouteDepartureTime", {
     },
     checkRouteTime: function(timetables) {
         // this function checks any bus route departure times that is over
-        // if so, update routeTimeTablesToDisplay and call updateRouteTimeDOM function
+        // if so, updates currentTimeTables and call updateRouteTimeDOM function
         Log.log("checkRouteTime called.");
         let isTimePassed = false;
         for (route in timetables) {
